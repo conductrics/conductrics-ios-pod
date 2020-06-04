9da0e689f897ca54d89324a2992992bfe0f791b5
Jesse Granger <jesse_granger@fastmail.com>
2020-06-04 12:18:41 -0700

    ignoring 'Conductrics.xcodeproj/project.xcworkspace/'

---
+++ Conductrics/Conductrics.h
@@ -0,0 +1,19 @@
+//
+//  Conductrics.h
+//  Conductrics
+//
+//  Created by Jesse Granger on 6/4/20.
+//  Copyright © 2020 Conductrics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Conductrics.
+FOUNDATION_EXPORT double ConductricsVersionNumber;
+
+//! Project version string for Conductrics.
+FOUNDATION_EXPORT const unsigned char ConductricsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Conductrics/PublicHeader.h>
+
+
