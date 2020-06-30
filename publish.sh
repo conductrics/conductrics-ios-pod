3d60ef01a7ac7d5b107448517b41bb7715c80122
Jesse Granger <jesse_granger@fastmail.com>
2020-06-30 09:22:46 -0700

    stop on error in publish script

--- publish.sh
+++ publish.sh
@@ -17,13 +17,16 @@ fi
 
 GIT_TAG=pod-release-$NEW_VERSION
 
-echo Patching podspec...
-sed -i -e 's/spec.version *= *"\([0-9.]*\)"/spec.version = "'"$NEW_VERSION"'"/' Conductrics.podspec
-sed -i -e 's/:tag *=> *"[^"]*"/:tag => "'"$GIT_TAG"'"/' Conductrics.podspec
-
-echo Creating $GIT_TAG...
-git tag $GIT_TAG
-git push origin $GIT_TAG
-
-echo Publishing to pod repo...
-pod trunk push
+echo Updating VERSION \
+&& echo $NEW_VERSION > VERSION \
+&& echo Patching podspec... \
+&& sed -i ".bak" -e 's/spec.version *= *"\([0-9.]*\)"/spec.version = "'"$NEW_VERSION"'"/' Conductrics.podspec \
+&& sed -i "" -e 's/:tag *=> *"[^"]*"/:tag => "'"$GIT_TAG"'"/' Conductrics.podspec \
+&& rm Conductrics.podspec.bak \
+&& echo Creating $GIT_TAG... \
+&& git commit VERSION Conductrics.podspec -m "bump version to $NEW_VERSION" \
+&& git tag $GIT_TAG \
+&& git push origin $GIT_TAG \
+&& echo Publishing to pod repo... \
+&& pod trunk push \
+&& echo Finished.
